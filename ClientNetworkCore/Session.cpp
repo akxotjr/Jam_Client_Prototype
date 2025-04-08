5a16e0d2d9383ff4ccae6d4fb8bfe7fa19ef4140
akxotjr <mataeseok@gmail.com>
2025-04-08 17:50:45 +0900

    04/08 17:50

--- ClientNetworkCore/Session.cpp
+++ ClientNetworkCore/Session.cpp
@@ -4,7 +4,7 @@
 #include <boost/system/error_code.hpp>
 
 Session::Session(ServiceRef service, boost::asio::any_io_executor executor)
-	: _service(service), _executor(executor), _socket(executor), _recvBuffer(BUFFER_SIZE)
+	: _service(service), _executor(executor) /*_socket(executor),*//* _recvBuffer(BUFFER_SIZE)*/
 {
 	auto owner = _service.lock();
 	if (!owner) return;
@@ -14,10 +14,302 @@ Session::Session(ServiceRef service, boost::asio::any_io_executor executor)
 
 Session::~Session()
 {
+	//_socket.close();
+}
+
+//void Session::Send(SendBufferRef sendBuffer)
+//{
+//	if (IsConnected() == false)
+//		return;
+//
+//	bool registerSend = false;
+//
+//	{
+//		WRITE_LOCK
+//		_sendQueue.push(sendBuffer); // how about change to lock-free queue?
+//
+//		if (_sendRegistered.exchange(true) == false)
+//			registerSend = true;
+//	}
+//
+//	if (registerSend)
+//		DoSend();
+//}
+
+
+//void Session::DoSend()
+//{
+//	if (IsConnected() == false || _socket.is_open() == false)
+//		return;
+//
+//	vector<boost::asio::const_buffer> sendBuffers;
+//
+//	{
+//		WRITE_LOCK
+//
+//		if (_sendQueue.empty())
+//			return;
+//
+//		int32 writeSize = 0;
+//		while (_sendQueue.empty() == false)
+//		{
+//			SendBufferRef sendBuffer = _sendQueue.front();
+//
+//			if (sendBuffer->Buffer() && sendBuffer->WriteSize() > 0)
+//			{
+//				writeSize += sendBuffer->WriteSize();
+//				_currentSendBuffers.push_back(sendBuffer);
+//				sendBuffers.push_back(boost::asio::buffer(sendBuffer->Buffer(), sendBuffer->WriteSize()));
+//			}
+//			_sendQueue.pop();
+//		}
+//	}
+//
+//	// Scatter-Gather
+//
+//	boost::asio::async_write(_socket, sendBuffers,
+//		[this](const boost::system::error_code& ec, std::size_t bytes_transferred) {
+//			if (!ec) 
+//			{
+//				std::cout << "Bytes sent: " << bytes_transferred << std::endl;
+//				_sendRegistered.store(false);
+//
+//				{
+//					WRITE_LOCK
+//					_currentSendBuffers.clear();
+//				}
+//
+//				OnSend(static_cast<int32>(bytes_transferred));
+//			}
+//			else 
+//			{
+//				std::cout << "Error: " << ec.message() << std::endl;
+//
+//				// retry send
+//				{
+//					WRITE_LOCK
+//					for (auto& buffer : _currentSendBuffers)
+//					{
+//						_sendQueue.push(buffer);
+//					}
+//					_currentSendBuffers.clear();
+//				}
+//			}
+//		});
+//}
+
+////void Session::DoRecv()  
+////{  
+////if (IsConnected() == false || _socket.is_open() == false)  
+////	return;  
+////
+////_recvBuffer.Clean();  
+////
+//////_socket.async_read_some(  
+//////	boost::asio::buffer(_recvBuffer.WritePos(), _recvBuffer.FreeSize()),  
+//////	[this](boost::system::error_code ec, size_t bytes_transferred) {  
+//////		if (!ec) {  
+//////			ProcessRecv(bytes_transferred);  
+//////		}  
+//////	});  
+////
+////	boost::asio::async_read(_socket, boost::asio::buffer(_recvBuffer.WritePos(), _recvBuffer.FreeSize()), boost::asio::transfer_at_least(2),  
+////		[this](const boost::system::error_code& ec, size_t bytes_transferred) {  
+////			if (!ec) {  
+////				ProcessRecv(bytes_transferred);  
+////			}  
+////		});  
+////}
+
+//void Session::DoRecvHeader()
+//{
+//	if (!IsConnected() || !_socket.is_open())
+//		return;
+//
+//	_recvBuffer.Clean();
+//
+//	// 헤더 크기만큼 정확히 읽기
+//	boost::asio::async_read(_socket,
+//		boost::asio::buffer(_recvBuffer.WritePos(), sizeof(PacketHeader)),
+//		boost::asio::transfer_exactly(sizeof(PacketHeader)),
+//		[this](boost::system::error_code ec, size_t bytes_transferred)
+//		{
+//			if (!ec)
+//			{
+//				_recvBuffer.OnWrite(bytes_transferred);
+//				PacketHeader* header = reinterpret_cast<PacketHeader*>(_recvBuffer.ReadPos());
+//				DoRecvBody(header->size - sizeof(PacketHeader));
+//			}
+//			else
+//			{
+//				std::cout << "Recv header error: " << ec.message() << "\n";
+//				Disconnect(ec.message());
+//			}
+//		});
+//}
+//
+//void Session::DoRecvBody(int32 bodySize)
+//{
+//	if (!IsConnected() || !_socket.is_open())
+//		return;
+//
+//	// 본문 크기만큼 정확히 읽기
+//	boost::asio::async_read(_socket,
+//		boost::asio::buffer(_recvBuffer.WritePos(), bodySize),
+//		boost::asio::transfer_exactly(bodySize),
+//		[this, bodySize](boost::system::error_code ec, size_t bytes_transferred)
+//		{
+//			if (!ec)
+//			{
+//				_recvBuffer.OnWrite(bytes_transferred);
+//
+//				ProcessRecv(sizeof(PacketHeader) + bodySize);
+//			}
+//			else
+//			{
+//				std::cout << "Recv body error: " << ec.message() << "\n";
+//				Disconnect(ec.message());
+//			}
+//		});
+//}
+//
+//void Session::ProcessRecv(size_t bytes_transferred)
+//{
+//	if (bytes_transferred == 0)
+//	{
+//		Disconnect("Recv 0");
+//		return;
+//	}
+//
+//	//if (_recvBuffer.OnWrite(bytes_transferred) == false)
+//	//{
+//	//	Disconnect("OnWrite Overflow");
+//	//	return;
+//	//}
+//
+//	int32 dataSize = _recvBuffer.DataSize();
+//	int32 processLen = OnRecv(_recvBuffer.ReadPos(), dataSize);
+//
+//	if (processLen < 0 || dataSize < processLen || _recvBuffer.OnRead(processLen) == false)
+//	{
+//		Disconnect("OnRead Overflow");
+//		return;
+//	}
+//
+//	//_recvBuffer.Clean();
+//
+//	DoRecvHeader();
+//}
+
+//void Session::Connect()
+//{
+//	tcp::resolver resolver(_executor);
+//
+//	auto endpoints = resolver.resolve(_netAddress.ip, _netAddress.port);
+//
+//	bool result = true;
+//
+//	auto service = _service.lock();
+//	
+//
+//	boost::asio::async_connect(_socket, endpoints,
+//		[&result, this](const boost::system::error_code& ec, const tcp::endpoint& endpoint) {
+//			if (!ec) 
+//			{
+//				std::cout << "Connected to: " << endpoint << std::endl;
+//				_connected.store(true);
+//
+//				DoRecvHeader();
+//
+//				OnConnected();
+//				//DoRecv();
+//			}
+//			else 
+//			{
+//				std::cout << "Connection failed: " << ec.message() << std::endl;
+//				OnConnected();
+//			}
+//		});
+//}
+//
+//void Session::Disconnect(const string cause)
+//{
+//	if (_connected.exchange(false) == false)
+//		return;
+//
+//	cout << "Disconnect : " << cause << std::endl;
+//
+//	if (_socket.is_open())
+//	{
+//		boost::system::error_code ec;
+//		_socket.cancel(ec);
+//		_socket.close(ec);
+//		if (ec)
+//		{
+//			std::cout << "Socket Close Error : " << ec.message() << std::endl;
+//		}
+//		else
+//		{
+//			OnDisconnected();
+//		}
+//	}
+//
+//	{
+//		WRITE_LOCK
+//
+//		_sendQueue = {};
+//		_currentSendBuffers.clear();
+//		_sendRegistered.store(false);
+//
+//		// TODO
+//	}
+//}
+
+
+//PacketSession::PacketSession(ServiceRef service, boost::asio::any_io_executor executor)
+//	: Session(service, executor)
+//{
+//
+//}
+//
+//PacketSession::~PacketSession()
+//{
+//}
+//
+//int32 PacketSession::OnRecv(BYTE* buffer, int32 len)
+//{
+//	int32 processLen = 0;
+//
+//	while (true)
+//	{
+//		int32 dataSize = len - processLen;
+//
+//		if (dataSize < sizeof(PacketHeader))
+//			break;
+//
+//		PacketHeader header = *reinterpret_cast<PacketHeader*>(&buffer[processLen]);
+//
+//		if (dataSize < header.size)
+//			break;
+//
+//		OnRecvPacket(&buffer[0], header.size);
+//		processLen += header.size;
+//	}
+//	return processLen;
+//}
+
+TcpSession::TcpSession(ServiceRef service, boost::asio::any_io_executor executor)
+	: Session(service, executor), _socket(executor), _recvBuffer(BUFFER_SIZE)
+{
+
+}
+
+TcpSession::~TcpSession()
+{
 	_socket.close();
 }
 
-void Session::Send(SendBufferRef sendBuffer)
+void TcpSession::Send(SendBufferRef sendBuffer)
 {
 	if (IsConnected() == false)
 		return;
@@ -33,11 +325,81 @@ void Session::Send(SendBufferRef sendBuffer)
 	}
 
 	if (registerSend)
-		DoSend();
+		RegisterSend();
 }
 
+void TcpSession::Connect()
+{
+	RegisterConnect();
+}
 
-void Session::DoSend()
+void TcpSession::Disconnect(const string cause)
+{
+	if (_connected.exchange(false) == false)
+		return;
+
+	RegisterDisconnect();
+}
+
+void TcpSession::RegisterConnect()
+{
+	tcp::resolver resolver(_executor);
+
+	auto endpoints = resolver.resolve(_netAddress.ip, _netAddress.port);
+
+	bool result = true;
+
+	auto service = _service.lock();
+
+
+	boost::asio::async_connect(_socket, endpoints,
+		[&result, this](const boost::system::error_code& ec, const tcp::endpoint& endpoint) {
+			if (!ec)
+			{
+				std::cout << "Connected to: " << endpoint << std::endl;
+				_connected.store(true);
+
+				RegisterRecvHeader();
+
+				OnConnected();
+			}
+			else
+			{
+				std::cout << "Connection failed: " << ec.message() << std::endl;
+				OnConnected();
+			}
+		});
+}
+
+void TcpSession::RegisterDisconnect()
+{
+	if (_socket.is_open())
+	{
+		boost::system::error_code ec;
+		_socket.cancel(ec);
+		_socket.close(ec);
+		if (ec)
+		{
+			std::cout << "Socket Close Error : " << ec.message() << std::endl;
+		}
+		else
+		{
+			OnDisconnected();
+		}
+	}
+
+	{
+		WRITE_LOCK
+
+			_sendQueue = {};
+		_currentSendBuffers.clear();
+		_sendRegistered.store(false);
+
+		// TODO
+	}
+}
+
+void TcpSession::RegisterSend()
 {
 	if (IsConnected() == false || _socket.is_open() == false)
 		return;
@@ -47,8 +409,8 @@ void Session::DoSend()
 	{
 		WRITE_LOCK
 
-		if (_sendQueue.empty())
-			return;
+			if (_sendQueue.empty())
+				return;
 
 		int32 writeSize = 0;
 		while (_sendQueue.empty() == false)
@@ -69,59 +431,36 @@ void Session::DoSend()
 
 	boost::asio::async_write(_socket, sendBuffers,
 		[this](const boost::system::error_code& ec, std::size_t bytes_transferred) {
-			if (!ec) 
+			if (!ec)
 			{
 				std::cout << "Bytes sent: " << bytes_transferred << std::endl;
 				_sendRegistered.store(false);
 
 				{
 					WRITE_LOCK
-					_currentSendBuffers.clear();
+						_currentSendBuffers.clear();
 				}
 
 				OnSend(static_cast<int32>(bytes_transferred));
 			}
-			else 
+			else
 			{
 				std::cout << "Error: " << ec.message() << std::endl;
 
 				// retry send
 				{
 					WRITE_LOCK
-					for (auto& buffer : _currentSendBuffers)
-					{
-						_sendQueue.push(buffer);
-					}
+						for (auto& buffer : _currentSendBuffers)
+						{
+							_sendQueue.push(buffer);
+						}
 					_currentSendBuffers.clear();
 				}
 			}
 		});
 }
 
-//void Session::DoRecv()  
-//{  
-//if (IsConnected() == false || _socket.is_open() == false)  
-//	return;  
-//
-//_recvBuffer.Clean();  
-//
-////_socket.async_read_some(  
-////	boost::asio::buffer(_recvBuffer.WritePos(), _recvBuffer.FreeSize()),  
-////	[this](boost::system::error_code ec, size_t bytes_transferred) {  
-////		if (!ec) {  
-////			ProcessRecv(bytes_transferred);  
-////		}  
-////	});  
-//
-//	boost::asio::async_read(_socket, boost::asio::buffer(_recvBuffer.WritePos(), _recvBuffer.FreeSize()), boost::asio::transfer_at_least(2),  
-//		[this](const boost::system::error_code& ec, size_t bytes_transferred) {  
-//			if (!ec) {  
-//				ProcessRecv(bytes_transferred);  
-//			}  
-//		});  
-//}
-
-void Session::DoRecvHeader()
+void TcpSession::RegisterRecvHeader()
 {
 	if (!IsConnected() || !_socket.is_open())
 		return;
@@ -138,7 +477,7 @@ void Session::DoRecvHeader()
 			{
 				_recvBuffer.OnWrite(bytes_transferred);
 				PacketHeader* header = reinterpret_cast<PacketHeader*>(_recvBuffer.ReadPos());
-				DoRecvBody(header->size - sizeof(PacketHeader));
+				RegisterRecvBody(header->size - sizeof(PacketHeader));
 			}
 			else
 			{
@@ -148,7 +487,7 @@ void Session::DoRecvHeader()
 		});
 }
 
-void Session::DoRecvBody(int32 bodySize)
+void TcpSession::RegisterRecvBody(int32 bodySize)
 {
 	if (!IsConnected() || !_socket.is_open())
 		return;
@@ -173,9 +512,14 @@ void Session::DoRecvBody(int32 bodySize)
 		});
 }
 
-void Session::ProcessRecv(size_t bytes_transferred)
+void TcpSession::ProcessSend(int32 numOfBytes)
 {
-	if (bytes_transferred == 0)
+
+}
+
+void TcpSession::ProcessRecv(int32 numOfBytes)
+{
+	if (numOfBytes == 0)
 	{
 		Disconnect("Recv 0");
 		return;
@@ -188,7 +532,7 @@ void Session::ProcessRecv(size_t bytes_transferred)
 	//}
 
 	int32 dataSize = _recvBuffer.DataSize();
-	int32 processLen = OnRecv(_recvBuffer.ReadPos(), dataSize);
+	int32 processLen = IsParsingPacket(_recvBuffer.ReadPos(), dataSize);
 
 	if (processLen < 0 || dataSize < processLen || _recvBuffer.OnRead(processLen) == false)
 	{
@@ -196,86 +540,12 @@ void Session::ProcessRecv(size_t bytes_transferred)
 		return;
 	}
 
-	//_recvBuffer.Clean();
+	_recvBuffer.Clean();
 
-	DoRecvHeader();
+	RegisterRecvHeader();
 }
 
-void Session::Connect()
-{
-	tcp::resolver resolver(_executor);
-
-	auto endpoints = resolver.resolve(_netAddress.ip, _netAddress.port);
-
-	bool result = true;
-
-	auto service = _service.lock();
-	
-
-	boost::asio::async_connect(_socket, endpoints,
-		[&result, this](const boost::system::error_code& ec, const tcp::endpoint& endpoint) {
-			if (!ec) 
-			{
-				std::cout << "Connected to: " << endpoint << std::endl;
-				_connected.store(true);
-
-				DoRecvHeader();
-
-				OnConnected();
-				//DoRecv();
-			}
-			else 
-			{
-				std::cout << "Connection failed: " << ec.message() << std::endl;
-				OnConnected();
-			}
-		});
-}
-
-void Session::Disconnect(const string cause)
-{
-	if (_connected.exchange(false) == false)
-		return;
-
-	cout << "Disconnect : " << cause << std::endl;
-
-	if (_socket.is_open())
-	{
-		boost::system::error_code ec;
-		_socket.cancel(ec);
-		_socket.close(ec);
-		if (ec)
-		{
-			std::cout << "Socket Close Error : " << ec.message() << std::endl;
-		}
-		else
-		{
-			OnDisconnected();
-		}
-	}
-
-	{
-		WRITE_LOCK
-
-		_sendQueue = {};
-		_currentSendBuffers.clear();
-		_sendRegistered.store(false);
-
-		// TODO
-	}
-}
-
-
-PacketSession::PacketSession(ServiceRef service, boost::asio::any_io_executor executor)
-	: Session(service, executor)
-{
-}
-
-PacketSession::~PacketSession()
-{
-}
-
-int32 PacketSession::OnRecv(BYTE* buffer, int32 len)
+int32 TcpSession::IsParsingPacket(BYTE* buffer, int32 len)
 {
 	int32 processLen = 0;
 
@@ -291,8 +561,152 @@ int32 PacketSession::OnRecv(BYTE* buffer, int32 len)
 		if (dataSize < header.size)
 			break;
 
-		OnRecvPacket(&buffer[0], header.size);
+		OnRecv(&buffer[0], header.size);
 		processLen += header.size;
 	}
 	return processLen;
+}
+
+
+
+ReliableUdpSession::ReliableUdpSession(ServiceRef service, boost::asio::any_io_executor executor)
+	: Session(service, executor), _socket(executor), _recvBuffer(BUFFER_SIZE)
+{
+}
+
+ReliableUdpSession::~ReliableUdpSession()
+{
+	_socket.close();
+}
+
+void ReliableUdpSession::Send(SendBufferRef sendBuffer)
+{
+
+}
+
+void ReliableUdpSession::Connect()
+{
+	RegisterConnect();
+}
+
+void ReliableUdpSession::Disconnect(const string cause)
+{
+}
+
+void ReliableUdpSession::RegisterSend()
+{
+	if (_socket.is_open() == false)
+		return;
+
+	if (_sendRegistered.exchange(true))
+		return;
+
+	if (_sendQueue.empty())
+	{
+		_sendRegistered = false;
+		return;
+	}
+
+	Vector<boost::asio::const_buffer> sendBuffers;
+
+	_currentSendBuffers.clear();
+
+	{
+		WRITE_LOCK
+
+		int32 writeSize = 0;
+		while (!_sendQueue.empty())
+		{
+			SendBufferRef sendBuffer = _sendQueue.front();
+			if (sendBuffer->Buffer() && sendBuffer->WriteSize() > 0)
+			{
+				writeSize += sendBuffer->WriteSize();
+				_currentSendBuffers.push_back(sendBuffer);
+				sendBuffers.push_back(boost::asio::buffer(sendBuffer->Buffer(), sendBuffer->WriteSize()));
+			}
+			_sendQueue.pop();
+		}
+	}
+
+	_socket.async_send_to(sendBuffers, *_remoteEndpoint.begin(),
+		[this, self = shared_from_this()](const boost::system::error_code& ec, size_t bytes_transferred)
+		{
+			_sendRegistered.store(false);
+			if (!ec)
+			{
+				ProcessSend();
+			}
+			else
+			{
+				//Disconnect("Failed to send");
+				{
+					WRITE_LOCK
+					for (auto& buffer : _currentSendBuffers)
+					{
+						_sendQueue.push(buffer);
+					}
+					_currentSendBuffers.clear();
+				}
+			}
+		}
+	);
+}
+
+void ReliableUdpSession::RegisterRecv()
+{
+	if (_socket.is_open() == false)
+		return;
+
+
+	_socket.async_receive_from(
+		boost::asio::buffer(_recvBuffer.WritePos(), _recvBuffer.FreeSize()),
+		_recvBuffer.EndPoint(),
+		[this, self = shared_from_this()](const boost::system::error_code& ec, size_t bytes_transferred)
+		{
+			if (!ec && bytes_transferred > 0)
+			{
+				_recvBuffer.OnWrite(bytes_transferred);
+				ProcessRecv(); // 수신된 데이터 처리
+			}
+			else
+			{
+				std::cerr << "UDP Receive Error: " << ec.message() << std::endl;
+				RegisterDisconnect();
+			}
+
+			RegisterRecv();
+		}
+	);
+}
+
+
+
+void ReliableUdpSession::RegisterConnect()
+{
+	_socket.open(udp::v4());
+
+	udp::resolver resolver(_executor);
+	_endpoint = resolver.resolve(_netAddress.ip, _netAddress.port);
+
+	RegisterRecv();
+}
+
+void ReliableUdpSession::RegisterDisconnect()
+{
+}
+
+void ReliableUdpSession::ProcessSend()
+{
+}
+
+void ReliableUdpSession::ProcessRecv()
+{
+}
+
+void ReliableUdpSession::ProcessConnect()
+{
+}
+
+void ReliableUdpSession::ProcessDisconnect()
+{
 }
--- ClientNetworkCore/Session.h
+++ ClientNetworkCore/Session.h
@@ -4,9 +4,8 @@
 #include "Service.h"
 #include "RecvBuffer.h"
 
-
 using boost::asio::ip::tcp;
-
+using boost::asio::ip::udp;
 
 class Session : public enable_shared_from_this<Session>
 {
@@ -19,59 +18,55 @@ public:
 	virtual ~Session();
 
 public:
-	void					Send(SendBufferRef sendBuffer);
-	void					Connect();
-	void					Disconnect(const string cause);
+	virtual void					Send(SendBufferRef sendBuffer) = 0;
+	virtual void					Connect() = 0;
+	virtual void					Disconnect(const string cause) = 0;
 
 
-	shared_ptr<Service>		GetService() { return _service.lock(); }
-	void					SetService(shared_ptr<Service> service) { _service = service; }
+	shared_ptr<Service>				GetService() { return _service.lock(); }
+	void							SetService(shared_ptr<Service> service) { _service = service; }
 
-	void			SetNetAddress(NetAddress address) { _netAddress = address; }
-	NetAddress		GetAddress() { return _netAddress; }
-	//remove_reference_t<asio::basic_stream_socket<tcp>&> GetSocket() { return std::move(_socket); }
-	bool			IsConnected() { return _connected; }
-	SessionRef		GetSessionRef() { return static_pointer_cast<Session>(shared_from_this()); }
+	NetAddress						GetNetAddress() { return _netAddress; }
+	void							SetNetAddress(NetAddress address) { _netAddress = address; }
+	bool							IsConnected() { return _connected; }
+	SessionRef						GetSessionRef() { return static_pointer_cast<Session>(shared_from_this()); }
+	void							SetId(int32 id) { _id = id; }
+	int32							GetId() { return _id; }
 
-	//temp
-	void SetId(int32 id) { _id = id; }
-	int32 GetId() { return _id; }
-
-	void DoRecv();
+	//void DoRecv();
 
 	//temp
-	void DoRecvHeader();
-	void DoRecvBody(int32 bodySize);
+	//void DoRecvHeader();
+	//void DoRecvBody(int32 bodySize);
 
-	void ProcessRecv(size_t bytes_transferred);
+	//void ProcessRecv(size_t bytes_transferred);
 
-private:
-	void DoSend();
+//private:
+//	void DoSend();
 	
 
 protected:
-
 	virtual void	OnConnected() {}
 	virtual int32	OnRecv(BYTE* buffer, int32 len) { return len; }
 	virtual void	OnSend(int32 len) {}
 	virtual void	OnDisconnected() {}
 
-private:
+protected:
 	weak_ptr<Service>					_service;
-	tcp::socket							_socket;
+	//tcp::socket							_socket;
 	boost::asio::any_io_executor		_executor;
 	NetAddress							_netAddress;
 
 	Atomic<bool>						_connected = false;
 
-	int32 _id = -1;
+	uint32 _id = 0;
 
-private:
-	Atomic<bool>						_sendRegistered = false;
-	queue<SendBufferRef>				_sendQueue;
-	Vector<SendBufferRef>				_currentSendBuffers;
-
-	RecvBuffer							_recvBuffer;
+//private:
+//	Atomic<bool>						_sendRegistered = false;
+//	queue<SendBufferRef>				_sendQueue;
+//	Vector<SendBufferRef>				_currentSendBuffers;
+//
+//	RecvBuffer							_recvBuffer;
 };
 
 
@@ -81,15 +76,94 @@ struct PacketHeader
 	uint16 id;	// Protocol ID
 };
 
-class PacketSession : public Session
+//class PacketSession : public Session
+//{
+//public:
+//	PacketSession(ServiceRef service, boost::asio::any_io_executor executor);
+//	virtual ~PacketSession();
+//
+//	PacketSessionRef	GetPacketSessionRef() { return static_pointer_cast<PacketSession>(shared_from_this()); }
+//
+//protected:
+//	virtual int32		OnRecv(BYTE* buffer, int32 len) sealed;
+//	virtual void		OnRecvPacket(BYTE* buffer, int32 len) abstract;
+//};
+
+class TcpSession : public Session
 {
+	USE_LOCK
+
+	enum { BUFFER_SIZE = 0x10000 }; // 64KB
+
 public:
-	PacketSession(ServiceRef service, boost::asio::any_io_executor executor);
-	virtual ~PacketSession();
+	TcpSession(ServiceRef service, boost::asio::any_io_executor executor);
+	virtual ~TcpSession();
 
-	PacketSessionRef	GetPacketSessionRef() { return static_pointer_cast<PacketSession>(shared_from_this()); }
+	virtual void Send(SendBufferRef sendBuffer) override;
+	virtual void Connect() override;
+	virtual void Disconnect(const string cause) override;
 
-protected:
-	virtual int32		OnRecv(BYTE* buffer, int32 len) sealed;
-	virtual void		OnRecvPacket(BYTE* buffer, int32 len) abstract;
+private:
+	void RegisterConnect();
+	void RegisterDisconnect();
+	void RegisterSend();
+	void RegisterRecvHeader();
+	void RegisterRecvBody(int32 bodySize);
+
+	void ProcessSend(int32 numOfBytes);
+	void ProcessRecv(int32 numOfBytes);
+
+
+	int32 IsParsingPacket(BYTE* buffer, int32 len);
+
+private:
+	tcp::socket							_socket;
+
+	Atomic<bool>						_sendRegistered = false;
+	queue<SendBufferRef>				_sendQueue;
+	Vector<SendBufferRef>				_currentSendBuffers;
+
+	RecvBuffer							_recvBuffer;
+};
+
+
+struct UdpPacketHeader : public PacketHeader
+{
+	uint16 sequence;
+};
+
+class ReliableUdpSession : public Session
+{
+	USE_LOCK
+
+	enum { BUFFER_SIZE = 0x10000 }; // 64KB
+
+public:
+	ReliableUdpSession(ServiceRef service, boost::asio::any_io_executor executor);
+	virtual ~ReliableUdpSession();
+
+	virtual void Send(SendBufferRef sendBuffer) override;
+	virtual void Connect() override;
+	virtual void Disconnect(const string cause) override;
+
+private:
+	void RegisterSend();
+	void RegisterRecv();
+	void RegisterConnect();
+	void RegisterDisconnect();
+
+	void ProcessSend();
+	void ProcessRecv();
+	void ProcessConnect();
+	void ProcessDisconnect();
+
+private:
+	udp::socket _socket;
+    boost::asio::ip::basic_resolver_results<udp> _remoteEndpoint;
+
+	Atomic<bool> _sendRegistered = false;
+	queue<SendBufferRef> _sendQueue;
+	Vector<SendBufferRef> _currentSendBuffers;
+
+	RecvBuffer _recvBuffer;
 };
